d2b041fdcf808615a6c021c555bfb8db452453e3
liamdj <liamdj@princeton.edu>
2022-04-11 10:40:29 -0400

    Playing with sims

--- simulate_matchings.py
+++ simulate_matchings.py
@@ -5,11 +5,11 @@ import pandas as pd
 from numpy.random import default_rng
 rng = default_rng()
 from min_cost_flow import MatchingGraph
+from scipy.optimize import linear_sum_assignment
 
 def generate_utilities(quality):
-    sorted_utils = np.flip(np.sort(rng.random(quality.size)))
-    ordering = generate_ordering(quality)
-    return sorted_utils[ordering]
+    vals = rng.exponential(quality, quality.size)
+    return vals / np.sum(vals)
 
 def generate_ordering(quality):
     return rng.choice(np.arange(quality.size), quality.size, replace=False, p=quality)
@@ -20,48 +20,60 @@ def get_fixed_weights(student_qualities, course_qualities, student_weights, cour
     """
     n, m = student_qualities.size, course_qualities.size
 
-    course_info = pd.DataFrame({'Course': np.arange(m), 'Slots': [1]*m, 'Base weight': [0]*m, 'First weight': [0]*m})
+    # course_info = pd.DataFrame({'Course': np.arange(m), 'Slots': [1]*m, 'Base weight': [0]*m, 'First weight': [0]*m})
 
-    weights = np.zeros((number, m))
+    weights = np.zeros((number, m), dtype=int)
     for t in range(number):
         match_weights = np.zeros((n, m))
         for s in range(1, n):
-            match_weights[s, :] += student_weights[generate_ordering(course_qualities)]
+            match_weights[s, :] += student_weights[np.argsort(generate_ordering(course_qualities))]
         for c in range(m):
-            match_weights[:, c] += course_weights[generate_ordering(student_qualities)]
+            match_weights[:, c] += course_weights[np.argsort(generate_ordering(student_qualities))]
 
         for c in range(m):
-            fixed_matches = pd.DataFrame({'Course': [c], 'Student index': [0], 'Course index': [c]})
-            graph = MatchingGraph(match_weights, np.zeros(n), course_info, fixed_matches)
-            graph.solve()
-            weights[t, c] = graph.graph_weight()
+            # fixed_matches = pd.DataFrame({'Course': [c], 'Student index': [0], 'Course index': [c]})
+            # graph = MatchingGraph(match_weights, np.zeros(n), course_info, fixed_matches)
+            # graph.solve()
+            fixed = match_weights.copy()
+            fixed[1:, c] = -np.inf
+            row, col = linear_sum_assignment(fixed, maximize=True)
+            weights[t, c] = int(round(fixed[row, col].sum()))
    
     return weights
 
-def ranking_performance(utilities, ranking, fixed_weights, rank_weights):
+def match_results(ranking, fixed_weights, rank_weights):
     assert(np.all(np.sort(ranking) == np.arange(ranking.size)))
+    arr = fixed_weights + rank_weights[ranking][np.newaxis, :]
+    # randomly select among equal entries
+    return np.argmax(rng.random(arr.shape) * (arr == np.max(arr, axis=1)[:, np.newaxis]), axis=1)
 
-    matches = np.argmax(fixed_weights + rank_weights[ranking][np.newaxis, :], axis=1)
+def avg_utility(utilities, matches):
     return np.sum(utilities[matches]) / matches.size
 
-def try_reordering(match_utilities, student_qualities, course_qualities, student_weights, course_weights, trials):
-    fixed_weights = get_fixed_weights(student_qualities, course_qualities, student_weights, course_weights, trials)
+def default_ranking(utilities):
     # element i is index of the course with index i in ranked order list
-    ranked_index = np.flip(np.argsort(match_utilities))
+    ranked_index = np.flip(np.argsort(utilities))
     # element i is the index of course i in ranked order list
-    init_ranking = np.argsort(ranked_index)
+    return np.argsort(ranked_index)
 
+def reorder_ranking(init_ranking, rank_from, rank_to):
+    index = np.argwhere(init_ranking == rank_from)[0]
+    if rank_to > rank_from:
+        ranking = np.where((init_ranking <= rank_to) & (init_ranking > rank_from), init_ranking - 1, init_ranking)
+    else:
+        ranking = np.where((init_ranking >= rank_to) & (init_ranking < rank_from), init_ranking + 1, init_ranking)
+    ranking[index] += rank_to - rank_from
+    return ranking
+
+def try_reordering(match_utilities, fixed_weights, student_weights):
+    init_ranking = default_ranking(match_utilities)
     m = init_ranking.size
     results = np.zeros((m, m))
     for old_rank in range(m):
-        ci = ranked_index[old_rank]
         for new_rank in range(m):
-            if new_rank > old_rank:
-                ranking = np.where((init_ranking <= new_rank) & (init_ranking > old_rank), init_ranking - 1, init_ranking)
-            else:
-                ranking = np.where((init_ranking >= new_rank) & (init_ranking < old_rank), init_ranking + 1, init_ranking)
-            ranking[ci] += new_rank - old_rank
-            results[old_rank, new_rank] = ranking_performance(match_utilities, ranking, fixed_weights, student_weights)
+            ranking = reorder_ranking(init_ranking, old_rank, new_rank)
+            matches = match_results(ranking, fixed_weights, student_weights)
+            results[old_rank, new_rank] = avg_utility(match_utilities, matches)
     return results
 
 
